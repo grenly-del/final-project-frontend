67fbdb926986706439f383eaec4cffc84e2e860f
grenly-del <ardiand400@gmail.com>
2024-11-11 09:19:19 +0800

    membuat animasi transisi perpindahan halaman

--- src/config/router.route.tsx
+++ src/config/router.route.tsx
@@ -1,31 +1,52 @@
-import {Routes, Route, useLocation} from 'react-router-dom'
-import Header from '../components/header'
+import {Routes, Route, useLocation, Location} from 'react-router-dom'
 import HomePage from '../pages/homepage'
 import AboutPage from '../pages/about'
 import DetailsPage from '../pages/details'
 import ContactPage from '../pages/contact'
 import PageTransition from '../pages/pageTransition'
 import { AnimatePresence } from 'framer-motion'
-
-const ConfigRoutes = () => {
-    const location = useLocation()
-
-    
+import Header from '../components/header'
+const AppRoutes = () => {
+    const location:Location = useLocation()
     return (
-        <>
-        <AnimatePresence mode='wait'>
-            <Header />
-            <Routes location={location} key={location.pathname}>
-                
-                <Route path='/' element={<PageTransition><HomePage /></PageTransition>} />
-                <Route path='/about' element={<PageTransition><AboutPage /></PageTransition>} />
-                <Route path='/details' element={<PageTransition><DetailsPage /></PageTransition>} />
-                <Route path='/contact' element={<PageTransition><ContactPage /></PageTransition>} />
-            </Routes>
-        </AnimatePresence>
-        </>
-        
+        <AnimatePresence mode='wait' initial={true}>
+                <Header />
+                <Routes location={location} key={location.pathname}>
+                    <Route
+                    path="/"
+                    element={
+                        <PageTransition>
+                        <HomePage />
+                        </PageTransition>
+                    }
+                    />
+                    <Route
+                    path="/about"
+                    element={
+                        <PageTransition>
+                        <AboutPage />
+                        </PageTransition>
+                    }
+                    />
+                    <Route
+                    path="/details"
+                    element={
+                        <PageTransition>
+                        <DetailsPage />
+                        </PageTransition>
+                    }
+                    />
+                    <Route
+                    path="/contact"
+                    element={
+                        <PageTransition>
+                        <ContactPage />
+                        </PageTransition>
+                    }
+                    />
+                </Routes>
+            </AnimatePresence>
     )
 }
 
-export default ConfigRoutes
+export default AppRoutes
--- src/pages/pageTransition/index.tsx
+++ src/pages/pageTransition/index.tsx
@@ -1,14 +1,37 @@
-import {motion} from 'framer-motion'
-import { ReactNode } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { useLocation } from 'react-router-dom';
 
 interface PageTransitionProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
-const PageTransition:React.FC<PageTransitionProps> = ({children}) => {
-    return (
-        <section className='relative w-full h-[100vh]'>
-            <motion.div
+const PageTransition: React.FC<PageTransitionProps> = ({ children }) => {
+  // const location = useLocation();
+  // const [pageTitle, setPageTitle] = useState('');
+
+  
+
+  // useEffect(() => {
+  //   switch (location.pathname) {
+  //     case '/':
+  //       setPageTitle('Home');
+  //       break;
+  //     case '/about':
+  //       setPageTitle('About');
+  //       break;
+  //     case '/gallery':
+  //       setPageTitle('Gallery');
+  //       break;
+  //     default:
+  //       setPageTitle('Page');
+  //   }
+  // }, [location.pathname]);
+
+  return (
+    <div className="relative overflow-hidden w-full h-[100vh]">
+      {/* Overlay Masuk */}
+      <motion.div
         className="fixed inset-0 bg-gray-800 origin-bottom z-[99999999]"
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
@@ -24,7 +47,7 @@ const PageTransition:React.FC<PageTransitionProps> = ({children}) => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        transition={{ duration: 0.3 }}
+        transition={{ duration: 0.7 }}
       >
         {children}
       </motion.div>
@@ -32,15 +55,16 @@ const PageTransition:React.FC<PageTransitionProps> = ({children}) => {
       {/* Overlay Keluar */}
       <motion.div
         className="fixed inset-0 bg-gray-800 origin-top z-[99999999]"
-        initial={{ scaleY: 0 }}
+        initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
         transition={{
-          duration: 1.5,
+          duration: 1.2,
           ease: [0.22, 1, 0.36, 1],
-        }} />
-        </section>
-    )
-}
+        }}
+      />
+    </div>
+  );
+};
 
-export default PageTransition
+export default PageTransition;
